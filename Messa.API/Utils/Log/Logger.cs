7f68b8507d9986e691ff86b23377b11ed1657880
Shirro <sadikano10@gmaill.com>
2017-09-08 01:45:55 +0200

    gitignore changed to sync Debug Message folder

---
+++ Messa.API/Utils/Log/Logger.cs
@@ -0,0 +1,15 @@
+﻿using Messa.API.Utils.Enums;
+
+namespace Messa.API.Utils.Log
+{
+    public class Logger
+    {
+        public delegate void OnLogDelegate(string log);
+        public event OnLogDelegate OnLog;
+
+        public void Log(string log, LogMessageType entry = LogMessageType.Info)
+        {
+            OnLog?.Invoke(log);
+        }
+    }
+}
